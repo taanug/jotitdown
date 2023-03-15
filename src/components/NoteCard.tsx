0163a35de2faf30fca9ffc1acc48163efea2cdff
StaticGold <staticgold@StaticGolds-iMac.local>
2023-03-15 03:55:22 -0700

    Layout updates

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -15,7 +15,7 @@ export const NoteCard = ({
   onDelete: () => void;
   onUpdate: (title: string, content: string) => void;
 }) => {
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [isDeleteAlertOpen, setIsDeleteAlertOpen] = useState(false);
   const [isEditorOpen, setIsEditorOpen] = useState(false);
 
   const [isExpanded, setIsExpanded] = useState(false);
@@ -25,10 +25,10 @@ export const NoteCard = ({
         <div>
           <div
             className={`collapse-arrow cursor-pointer ${
-              isExpanded || showDeleteAlert ? "collapse-open" : ""
+              isExpanded || isDeleteAlertOpen ? "collapse-open" : ""
             } collapse`}
             onClick={() => {
-              if (showDeleteAlert) {
+              if (isDeleteAlertOpen) {
                 return;
               }
               setIsExpanded(!isExpanded);
@@ -61,30 +61,41 @@ export const NoteCard = ({
               />
             </div>
           )}
-          {!showDeleteAlert && isExpanded && (
-            <div
-              className="btn-warning btn-xs btn ml-3 mb-3 px-5"
-              onClick={(e) => {
-                setShowDeleteAlert(true);
-                setIsExpanded(true);
-              }}
-            >
-              Delete
-            </div>
-          )}
-          {!isEditorOpen && isExpanded && (
-            <div
-              className="btn-warning btn-xs btn ml-3 mb-3 px-5"
-              onClick={(e) => {
-                setIsEditorOpen(true);
-                setIsExpanded(true);
-              }}
-            >
-              Edit
-            </div>
-          )}
-          <div>
-            {showDeleteAlert && (
+          <div className="flex w-full">
+            {!isEditorOpen && !isDeleteAlertOpen && isExpanded && (
+              <div
+                className="btn-primary btn-xs btn m-auto ml-3 mb-3 px-5"
+                onClick={(e) => {
+                  setIsEditorOpen(true);
+                  setIsExpanded(true);
+                }}
+              >
+                Edit
+              </div>
+            )}
+            {!isDeleteAlertOpen && isExpanded && (
+              <svg
+                onClick={(e) => {
+                  setIsDeleteAlertOpen(true);
+                  setIsExpanded(true);
+                }}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="mr-3 ml-auto h-6 w-6 cursor-pointer"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                />
+              </svg>
+            )}
+          </div>
+          <div className="ml-3 mr-3">
+            {isDeleteAlertOpen && (
               <div className="alert alert-warning shadow-lg">
                 <div>
                   <svg
@@ -108,7 +119,7 @@ export const NoteCard = ({
                   <button
                     className="btn-primary btn-sm btn z-50"
                     onClick={(e) => {
-                      setShowDeleteAlert(!showDeleteAlert);
+                      setIsDeleteAlertOpen(!isDeleteAlertOpen);
                       setIsExpanded(false);
                     }}
                   >
@@ -118,7 +129,7 @@ export const NoteCard = ({
                     className="btn-ghost btn-sm btn z-50"
                     onClick={(e) => {
                       onDelete();
-                      setShowDeleteAlert(!showDeleteAlert);
+                      setIsDeleteAlertOpen(!isDeleteAlertOpen);
                       setIsExpanded(false);
                     }}
                   >
